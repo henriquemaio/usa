c868e808b54901b7a91b5d8cc458c060a3af49b1
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2016-07-17 09:35:37 -0300

    Adding functions for front-end utility;

--- usa/public/javascripts/scripts.js
+++ usa/public/javascripts/scripts.js
@@ -1,250 +1,274 @@
-var Clinton = 211;
-var trump = 164;
-var neutral = 163;
-
 $( document ).ready(function() {
-    updateDiverging();
-    
-    var tooltips = document.querySelectorAll('.myTooltip .toolTip');
+  updateDiverging();
 
-    window.onmousemove = function (e) {
-        var x = (e.clientX - 20) + 'px',
-            y = (e.clientY + 20) + 'px';
-        for (var i = 0; i < tooltips.length; i++) {
-            tooltips[i].style.top = y;
-            tooltips[i].style.left = x;
-        }
-    };
-    
+  var tooltips = document.querySelectorAll('.myTooltip .toolTip');
+
+  window.onmousemove = function (e) {
+    var x = (e.clientX - 20) + 'px',
+        y = (e.clientY + 20) + 'px';
+    for (var i = 0; i < tooltips.length; i++) {
+      tooltips[i].style.top = y;
+      tooltips[i].style.left = x;
+    }
+  };
+
 });
 
 function updateDiverging() {
-    
-    setTimeout(function () {
-        total = Clinton + trump + neutral;
-        parcialClinton = (Clinton * 100)/total;
-        parcialTrump = (trump * 100)/total;
-        parcialNeutral = (neutral * 100)/total;
-        console.log(parcialClinton);
-        console.log(parcialNeutral);
-        console.log(parcialTrump);
-        $('#divergingClinton').width(parcialClinton+'%');
-        $('#divergingNeutral').width(parcialNeutral+'%');
-        $('#divergingTrump').width(parcialTrump+'%');
-    }, 100);
-    
+  $.get('/api/total', function(json){
+    data = json.payload;
+    var clinton = data.hillary.count;
+    var trump = data.trump.count;
+    var neutral = data.void.count;
+    total = clinton + trump + neutral;
+    parcialClinton = (clinton * 100)/total;
+    parcialTrump = (trump * 100)/total;
+    parcialNeutral = (neutral * 100)/total;
+    $('#divergingClinton').width(parcialClinton+'%');
+    $('#divergingNeutral').width(parcialNeutral+'%');
+    $('#divergingTrump').width(parcialTrump+'%');
+    $('#indexesTrump .number').text(trump);
+    $('#indexesNeutral .number').text(neutral);
+    $('#indexesClinton .number').text(clinton);
+  });
 }
 
 $(function () {
-		/**
- * Sand-Signika theme for Highcharts JS
- * @author Torstein Honsi
- */
+	/**
+   * Sand-Signika theme for Highcharts JS
+   * @author Torstein Honsi
+   */
 
-// Load the fonts
-Highcharts.createElement('link', {
-   href: 'https://fonts.googleapis.com/css?family=Signika:400,700',
-   rel: 'stylesheet',
-   type: 'text/css'
-}, null, document.getElementsByTagName('head')[0]);
+  // Load the fonts
+  Highcharts.createElement('link', {
+    href: 'https://fonts.googleapis.com/css?family=Signika:400,700',
+    rel: 'stylesheet',
+    type: 'text/css'
+  }, null, document.getElementsByTagName('head')[0]);
 
-// Add the background image to the container
-Highcharts.wrap(Highcharts.Chart.prototype, 'getContainer', function (proceed) {
-   proceed.call(this);
-   this.container.style.background = 'url(chartbg.png)';
-});
+  // Add the background image to the container
+  Highcharts.wrap(Highcharts.Chart.prototype, 'getContainer', function (proceed) {
+    proceed.call(this);
+    this.container.style.background = 'url(chartbg.png)';
+  });
 
 
-Highcharts.theme = {
-   colors: ["#f45b5b", "#8085e9", "#8d4654", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
-      "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
-   chart: {
+  Highcharts.theme = {
+    colors: ["#f45b5b", "#8085e9", "#8d4654", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+             "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
+    chart: {
       backgroundColor: null,
       style: {
-         fontFamily: "Signika, serif"
+        fontFamily: "Signika, serif"
       }
-   },
-   title: {
+    },
+    title: {
       style: {
-         color: 'black',
-         fontSize: '16px',
-         fontWeight: 'bold'
+        color: 'black',
+        fontSize: '16px',
+        fontWeight: 'bold'
       }
-   },
-   subtitle: {
+    },
+    subtitle: {
       style: {
-         color: 'black'
+        color: 'black'
       }
-   },
-   tooltip: {
+    },
+    tooltip: {
       borderWidth: 0
-   },
-   legend: {
+    },
+    legend: {
       itemStyle: {
-         fontWeight: 'bold',
-         fontSize: '13px'
+        fontWeight: 'bold',
+        fontSize: '13px'
       }
-   },
-   xAxis: {
+    },
+    xAxis: {
       labels: {
-         style: {
-            color: '#6e6e70'
-         }
+        style: {
+          color: '#6e6e70'
+        }
       }
-   },
-   yAxis: {
+    },
+    yAxis: {
       labels: {
-         style: {
-            color: '#6e6e70'
-         }
+        style: {
+          color: '#6e6e70'
+        }
       }
-   },
-   plotOptions: {
+    },
+    plotOptions: {
       series: {
-         shadow: true
+        shadow: true
       },
       candlestick: {
-         lineColor: '#404048'
+        lineColor: '#404048'
       },
       map: {
-         shadow: false
+        shadow: false
       }
-   },
+    },
 
-   // Highstock specific
-   navigator: {
+    // Highstock specific
+    navigator: {
       xAxis: {
-         gridLineColor: '#D0D0D8'
+        gridLineColor: '#D0D0D8'
       }
-   },
-   rangeSelector: {
+    },
+    rangeSelector: {
       buttonTheme: {
-         fill: 'white',
-         stroke: '#C0C0C8',
-         'stroke-width': 1,
-         states: {
-            select: {
-               fill: '#D0D0D8'
-            }
-         }
+        fill: 'white',
+        stroke: '#C0C0C8',
+        'stroke-width': 1,
+        states: {
+          select: {
+            fill: '#D0D0D8'
+          }
+        }
       }
-   },
-   scrollbar: {
+    },
+    scrollbar: {
       trackBorderColor: '#C0C0C8'
-   },
+    },
 
-   // General
-   background2: '#E0E0E8'
+    // General
+    background2: '#E0E0E8'
 
-};
+  };
 
-// Apply the theme
-Highcharts.setOptions(Highcharts.theme);
-  
-    // Get the CSV and create the chart
-  
-    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=analytics.csv&callback=?', function (csv) {
+  // Apply the theme
+  Highcharts.setOptions(Highcharts.theme);
 
-        $('#container').highcharts({
+  // Get the CSV and create the chart
+  var month = 7;
+  var interval = 30 * 24 * 3600 * 1000;
+  $.getJSON('/api/bymonth/'+ month , function (json) {
 
-            data: {
-                csv: csv
-            },
+    loadCharts(json.payload.csv, interval, loadDay);
 
-            title: {
-                text: 'Hillary Clinton x Donald Trump'
-            },
 
-            subtitle: {
-                text: 'Fonte: Twitter'
-            },
-
-            xAxis: {
-                tickInterval: 7 * 24 * 3600 * 1000, // one week
-                tickWidth: 0,
-                gridLineWidth: 1,
-                labels: {
-                    align: 'left',
-                    x: 3,
-                    y: -3
-                }
-            },
-
-            yAxis: [{ // left y axis
-                title: {
-                    text: null
-                },
-                labels: {
-                    align: 'left',
-                    x: 3,
-                    y: 16,
-                    format: '{value:.,0f}'
-                },
-                showFirstLabel: false
-            }, { // right y axis
-                linkedTo: 0,
-                gridLineWidth: 0,
-                opposite: true,
-                title: {
-                    text: null
-                },
-                labels: {
-                    align: 'right',
-                    x: -3,
-                    y: 16,
-                    format: '{value:.,0f}'
-                },
-                showFirstLabel: false
-            }],
-
-            legend: {
-                align: 'left',
-                verticalAlign: 'top',
-                y: 20,
-                floating: true,
-                borderWidth: 0
-            },
-
-            tooltip: {
-                shared: true,
-                crosshairs: true
-            },
-
-            plotOptions: {
-                series: {
-                    cursor: 'pointer',
-                    point: {
-                        events: {
-                            click: function (e) {
-                                hs.htmlExpand(null, {
-                                    pageOrigin: {
-                                        x: e.pageX || e.clientX,
-                                        y: e.pageY || e.clientY
-                                    },
-                                    headingText: this.series.name,
-                                    maincontentText: Highcharts.dateFormat('%A, %b %e, %Y', this.x) + ':<br/> ' +
-                                        this.y + ' visits',
-                                    width: 200
-                                });
-                            }
-                        }
-                    },
-                    marker: {
-                        lineWidth: 1
-                    }
-                }
-            },
-
-            series: [{
-                name: 'All visits',
-                lineWidth: 4,
-                marker: {
-                    radius: 4
-                }
-            }, {
-                name: 'USA Fight'
-            }]
-        });
-    });
+  });
 
 });
+
+
+function loadDay(e) {
+  console.log(this.x,this.y);
+  var localDate = new Date(this.x);
+  var offset = localDate.getTimezoneOffset() * 60000;
+  var date = new Date(localDate.getTime() + offset);
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  console.log(date,day,month);
+  var interval = 3600 * 1000;
+  $.getJSON('/api/byday/'+ month + '/'+ day, function (json) {
+    console.log(json);
+    loadCharts(json.payload.csv, interval, showData);
+  });
+}
+
+function showData(e) {
+  hs.htmlExpand(null, {
+    pageOrigin: {
+      x: e.pageX || e.clientX,
+      y: e.pageY || e.clientY
+    },
+    headingText: this.series.name,
+    maincontentText: Highcharts.dateFormat('%A, %b %e, %Y', this.x) + ':<br/> ' +
+      this.y + ' sentiment',
+    width: 200
+  });
+}
+
+function loadCharts(csv, interval, eventClick) {
+  $('#container').highcharts({
+
+    data: {
+      csv: csv
+    },
+
+    title: {
+      text: 'Hillary Clinton x Donald Trump'
+    },
+
+    subtitle: {
+      text: 'Fonte: Twitter'
+    },
+
+    xAxis: {
+      tickInterval: interval, // one week
+      tickWidth: 0,
+      gridLineWidth: 1,
+      labels: {
+        align: 'left',
+        x: 3,
+        y: -3
+      },
+      type: 'datetime'
+    },
+
+    yAxis: [{ // left y axis
+      title: {
+        text: null
+      },
+      labels: {
+        align: 'left',
+        x: 3,
+        y: 16,
+        format: '{value:.,0f}'
+      },
+      showFirstLabel: false
+    }, { // right y axis
+      linkedTo: 0,
+      gridLineWidth: 0,
+      opposite: true,
+      title: {
+        text: null
+      },
+      labels: {
+        align: 'right',
+        x: -3,
+        y: 16,
+        format: '{value:.,0f}'
+      },
+      showFirstLabel: false
+    }],
+
+    legend: {
+      align: 'left',
+      verticalAlign: 'top',
+      y: 20,
+      floating: true,
+      borderWidth: 0
+    },
+
+    tooltip: {
+      shared: true,
+      crosshairs: true
+    },
+
+    plotOptions: {
+      series: {
+        cursor: 'pointer',
+        point: {
+          events: {
+            click: eventClick
+          }
+        },
+        marker: {
+          lineWidth: 1
+        }
+      }
+    },
+
+    series: [{
+      name: 'All visits',
+      lineWidth: 4,
+      marker: {
+        radius: 4
+      }
+    }, {
+      name: 'USA Fight'
+    }]
+  });
+}
